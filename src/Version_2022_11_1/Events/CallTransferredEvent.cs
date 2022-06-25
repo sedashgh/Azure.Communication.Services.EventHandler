e70573f91a31115ae9cf944828babd1ed931377e
Jason Shave <28457536+jasonshave@users.noreply.github.com>
2022-06-25 07:49:43 -0600

    updated namespaces

--- src/Version_2022_11_1/Events/CallTransferredEvent.cs
+++ src/Version_2022_11_1/Events/CallTransferredEvent.cs
@@ -1,4 +1,4 @@
-﻿namespace JasonShave.Azure.Communication.Service.CallingServer.Extensions.Version_2022_11_1.Events;
+﻿namespace JasonShave.Azure.Communication.Service.CallingServer.Extensions.Events;
 
 public class CallTransferredEvent
 {
