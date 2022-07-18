c2859544f9937937e6a701a055a04f47cf54ebc4
Jason Shave <28457536+jasonshave@users.noreply.github.com>
2022-07-18 07:49:49 -0600

    updating IncomingCall to use CommunicationIdentifier from ACS common (#26)
    * updating IncomingCall to use CommunicationIdentifier from ACS common library.
    
    * updated models and included non-abstract type for CommunicationIdentifier

---
+++ src/CallingServer.Sdk.Contracts/Common/CommunicationIdentifierKind.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) 2022 Jason Shave. All rights reserved.
+// Licensed under the MIT License.
+
+namespace JasonShave.Azure.Communication.Service.CallingServer.Sdk.Contracts.Common;
+
+public enum CommunicationIdentifierKind
+{
+    Unknown,
+    CommunicationUser,
+    PhoneNumber,
+    MicrosoftTeamsUser,
+}
